da02a1484805dc3c6abb5bf1299d53edec218d76
Kaspek2480 <matipole@gmail.com>
2023-03-24 13:14:17 +0100

    Args showcase

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "Hello World" << endl;
+
+    cout << "Arg count: " << argc << endl;
+
+    for (int i = 0; i < argc; ++i) {
+        cout << "Arg " << i << ": " << argv[i] << endl;
+    }
+
     return 0;
 }
