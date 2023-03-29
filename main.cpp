42af1372de107856e5c900f65907c84590bf6a86
mrcinkrkwk1 <cholik@o2.pl>
2023-03-29 21:33:51 +0200

    test commit

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 4) {
-        cerr << "Blad liczby arg" << endl;
+    if (argc != 3) {
+        cout << "Blad liczby arg" << endl;
         return -1;
     }
 
     cout << argv[2] << endl;
 
-//    cout << "Arg count: " << argc << endl;
-//
-//    for (int i = 0; i < argc; ++i) {
-//        cout << "Arg " << i << ": " << argv[i] << endl;
-//    }
+    cout << "Arg count: " << argc << endl;
+
+    for (int i = 0; i < argc; ++i) {
+        cout << "Arg " << i << ": " << argv[i] << endl;
+    }
 
     return 0;
 }
