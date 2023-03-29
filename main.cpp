a859578bfff3cabd284285d3e7df31f1badfe2d2
mrcinkrkwk1 <cholik@o2.pl>
2023-03-29 22:59:33 +0200

    Added 2 loops in main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
     if (argc != 3) {
-        cout << "Blad liczby arg" << endl;
+        cerr << "Blad liczby argumentow" << endl;
         return -1;
-    }
+    } //sprawdzanie liczby argumentow
 
-    cout << argv[1] << endl;
 
-    cout << "Arg count: " << argc << endl;
+    for (int i = 0; i < argc; i++) {
+        if (fopen(argv[i], "r") == NULL) {
+            cout << "Katalog nie istnieje" << endl;
+            return -1;
+        }
+    } //sprawdzanie czy katalogi istnieja
 
-    for (int i = 0; i < argc; ++i) {
-        cout << "Arg " << i << ": " << argv[i] << endl;
-    }
+
+    for (int i = 0; i < argc; i++) {
+        if (std::filesystem::is_directory(argv[i]) == 0) {
+            cout << "Podana sciezka nie jest katalogiem" << endl;
+        } else {
+            cout << "Podana sciezka jest katalogiem" << endl;
+        }
+    } //sprawdzenie czy podane sciezki sa katalogiem
+
 
     return 0;
 }
